b892705c7f30eb976f731476a7550b08cf95f229
Marcin Drozd <marcindrozd@me.com>
2015-01-02 16:49:35 +0100

    Changed choices tohash instead of array and done some refactoring.

--- game.rb
+++ game.rb
@@ -1,46 +1,46 @@
 # Rock, paper, scissors
-possible_choices = ["P", "R", "S"]
+CHOICES = {"p" => "paper", "r" => "rock", "s" => "scissors"}
 
 puts "Play Paper Rock Scissors!"
 
 while true
   while true
-    puts "Choose one: (P/R/S)"
-    player_selected = gets.chomp.upcase
-    break if possible_choices.include? player_selected
+    puts "Choose one: (p/r/s)"
+    player_choice = gets.chomp.downcase
+    break if CHOICES.keys.include? player_choice
   end
 
-  computer_selected = possible_choices.sample # sample selects random value from array
+  computer_choice = CHOICES.keys.sample # sample selects random value from array
 
-  if computer_selected == player_selected
+  if computer_choice == player_choice
     puts "It's a tie!"
-  elsif player_selected == "P" && computer_selected == "S"
+  elsif player_choice == "p" && computer_choice == "s"
     puts "You picked Paper and computer picked Scissors."
     puts "Scissors cut through paper."
     puts "Computer wins!"
-  elsif player_selected == "S" && computer_selected == "R"
+  elsif player_choice == "s" && computer_choice == "r"
     puts "You picked Scissors and computer picked Rock."
     puts "Rock destroys Scissors."
     puts "Computer wins!"
-  elsif player_selected == "R" && computer_selected == "P"
+  elsif player_choice == "r" && computer_choice == "p"
     puts "You picked Rock and computer picked Paper."
     puts "Paper wraps Rock."
     puts "Computer wins!"
-  elsif player_selected == "P" && computer_selected == "R"
+  elsif player_choice == "p" && computer_choice == "r"
     puts "You picked Paper and computer picked Rock."
     puts "Paper wraps Rock."
     puts "Player wins!"
-  elsif player_selected == "S" && computer_selected == "P"
+  elsif player_choice == "s" && computer_choice == "p"
     puts "You picked Scissors and computer picked Paper."
     puts "Scissors cut through paper."
     puts "Player wins!"
-  elsif player_selected == "R" && computer_selected == "S"
+  elsif player_choice == "r" && computer_choice == "s"
     puts "You picked Rock and computer picked Scissors."
     puts "Rock destroys Scissors."
     puts "Player wins!"
   end
 
-  puts "Play again? (Y/N)"
-  response = gets.chomp.upcase
-  break if response == "N"
+  puts "Play again? (y/n)"
+  response = gets.chomp.downcase
+  break if response != "y"
 end
